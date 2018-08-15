ba13d92ab3a8dfeda7e425a001d3a048d65fa01b
DevasenaK <42206618+DevasenaK@users.noreply.github.com>
2018-08-15 00:39:11 -0500

    Add files via upload

---
+++ API/SchedulerController.js
@@ -0,0 +1,17 @@
+﻿app.controller('SchedulerController', function ($scope, $http) {
+    getAllSchedules();
+
+    function getAllSchedules() {
+        
+        $http({
+            method: 'GET',
+            url: 'http://localhost:63989/api/EmployeeScheduler/GetSchedules'
+        })
+            .then(function (response) {                 
+                $scope.result = response.data;           
+      
+
+            });
+
+    }
+})   
---
+++ API/Service.js
@@ -0,0 +1,7 @@
+﻿app.service("APIService", function ($http) {
+    var apiUrl = "http://localhost:63989/";
+    this.GetSchedules = function () {
+        var scheduleUrl = apiUrl + "api/EmployeeScheduler/GetSchedules";
+        return $http.get(scheduleUrl);
+    }
+});   
